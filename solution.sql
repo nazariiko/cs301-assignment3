58891691c5fd13e0b6521423a55c9b66f809df1b
nazarii.dev <nazaeii.dev@gmail.com>
2026-06-28 22:27:43 +0300

    1+2 tasks

--- solution.sql
+++ solution.sql
@@ -34,3 +34,43 @@ create table order_log (
     action varchar(50),
     log_date timestamp default current_timestamp
 );
+
+--------------------------------------------------------------
+--------------------------- Task 1 ---------------------------
+--------------------------------------------------------------
+
+create or replace function calculate_order_total(p_order_id int)
+returns int
+language plpgsql
+as $$
+declare order_total int;
+begin
+    select
+        sum(oi.price * oi.quantity) into order_total
+    from order_items oi
+    where oi.order_id = p_order_id;
+
+    return order_total;
+end;
+$$;
+
+select calculate_order_total(1);
+
+--------------------------------------------------------------
+--------------------------- Task 2 ---------------------------
+--------------------------------------------------------------
+
+create or replace procedure create_order(p_customer_id int)
+language plpgsql
+as $$
+begin
+    if exists(select 1 from customers c where c.customer_id = p_customer_id) then
+        insert into orders (customer_id, order_date, total_amount)
+        values (p_customer_id, now(), 0);
+    else
+        raise exception 'Клієнта з id=% не існує', p_customer_id;
+    end if;
+end;
+$$;
+
+call create_order(1);
