71762124fb9474527af2bc0165900a6f3488cf91
nazarii.dev <nazaeii.dev@gmail.com>
2026-06-29 00:12:38 +0300

    3-4 tasks

--- solution.sql
+++ solution.sql
@@ -43,7 +43,7 @@ create or replace function calculate_order_total(p_order_id int)
 returns int
 language plpgsql
 as $$
-declare order_total int;
+declare order_total numeric(10, 2);
 begin
     select
         sum(oi.price * oi.quantity) into order_total
@@ -74,3 +74,75 @@ end;
 $$;
 
 call create_order(1);
+
+--------------------------------------------------------------
+--------------------------- Task 3 ---------------------------
+--------------------------------------------------------------
+
+create or replace procedure add_product_to_order(p_order_id int, p_product_id int, p_quantity int)
+language plpgsql
+as $$
+declare
+    product_price numeric(10,2);
+    product_quantity int;
+begin
+    if p_quantity <= 0 then
+        raise exception 'Кількість не може бути менше або дорівнювати 0';
+    end if;
+
+    if not exists(select 1 from orders o where o.order_id = p_order_id) then
+        raise exception 'Замовлення з id=% не існує', p_order_id;
+    end if;
+
+    select
+        price, stock_quantity
+    into product_price, product_quantity
+    from products p
+    where p.product_id = p_product_id;
+
+    if not found then
+        raise exception 'Продукта з id=% не існує', p_product_id;
+    end if;
+
+    if p_quantity > product_quantity then
+        raise exception 'Продукта з id=% недостатньо на складі', p_product_id;
+    end if;
+
+    insert into order_items (order_id, product_id, quantity, price)
+    values (p_order_id, p_product_id, p_quantity, p_quantity * product_price);
+
+    update products
+    set stock_quantity = stock_quantity - 1
+    where product_id = p_product_id;
+end;
+$$;
+
+call add_product_to_order(1, 1, 2);
+
+--------------------------------------------------------------
+--------------------------- Task 4 ---------------------------
+--------------------------------------------------------------
+
+create or replace function recalculate_order_total()
+returns trigger
+language plpgsql
+as $$
+begin
+    if tg_op = 'DELETE' then
+        update orders
+        set    total_amount = calculate_order_total(old.order_id)
+        where  order_id = old.order_id;
+    else
+        update orders
+        set    total_amount = calculate_order_total(new.order_id)
+        where  order_id = new.order_id;
+    end if;
+
+    return null;
+end;
+$$;
+
+create or replace trigger trg_recalculate_order_total
+after insert or update or delete on order_items
+for each row
+execute function recalculate_order_total();
